fcd8ff69891d5686b05203f5510ad23704672cba
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-07-16 22:47:08 +0400

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,6 +9,7 @@ import './login-view.scss';
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginForm, setLoginform] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,26 +19,33 @@ export function LoginView(props) {
     props.onLoggedIn(username);
   };
 
+  const toggleForm = () => {
+    setLoginform(!loginForm)
+  }
+
   return (
     <div className="login-layout">
       <h3 className="text-center border border-dark shadow p-3 mb-5 bg-white rounded">myFlix movies!</h3>
-      <Form className="login-form">
-        <Form.Group controlId="formUsername">
-          <Form.Label className="font-weight-bold">Username:</Form.Label>
-          <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-        </Form.Group>
+      {
+        !loginForm ? <RegistrationView /> :
+          <Form className="login-form">
+            <Form.Group controlId="formUsername">
+              <Form.Label className="font-weight-bold">Username:</Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
 
-        <Form.Group controlId="formPassword">
-          <Form.Label className="font-weight-bold">Password:</Form.Label>
-          <Form.Control type="password" onChange={e => setPassword(e.target.value)} /></Form.Group>
-        <div className="login-bt">
-          <Button className="font-weight-bold" variant="success" size="lg" type="submit" onClick={handleSubmit}>Members Login</Button>
-        </div>
-        <h5 className="log-Reg">OR</h5>
-        <div className="register-bt">
-          <Button className="font-weight-bold" variant="warning" size="lg" type="submit" onClick={handleSubmit}>Register to become member</Button>
-        </div>
-      </Form>
+            <Form.Group controlId="formPassword">
+              <Form.Label className="font-weight-bold">Password:</Form.Label>
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} /></Form.Group>
+            <div className="login-bt">
+              <Button className="font-weight-bold" variant="success" size="lg" type="submit" onClick={handleSubmit}>Members Login</Button>
+            </div>
+            <h5 className="log-Reg">OR</h5>
+            <div className="register-bt">
+              <Button className="font-weight-bold" variant="warning" size="lg" type="submit" onClick={toggleForm}>Register to become a member</Button>
+            </div>
+          </Form>
+      }
     </div>
   );
 }
