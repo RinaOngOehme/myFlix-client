67c9cba5a1c670c5346c6bc9e196ed07f0d7f822
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-07-17 10:01:15 +0400

    Create genre-view.jsx

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './genre-view.scss';
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <div className="genre-view">
+        <div className="genre-name">
+          <span className="label">Name: </span>
+          <span className="value">{genre.Name}</span>
+        </div>
+        <div className="genre-description">
+          <span className="label">Description: </span>
+          <span className="value">{genre.Description}</span>
+        </div>
+        <Button variant="secondary" size="md" onClick={() => { onBackClick(null); }}>Back</Button>
+      </div>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+  }),
+  onBackClick: PropTypes.func.isRequired
+};
