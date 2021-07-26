0be65377b7bbfdd43afb9797f3c038b8d43a6931
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-07-26 17:34:28 +0400

    Create movies-list.jsx

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import PropTypes from 'prop-types';
+
+import { MovieCard } from '../movie-card/movie-card';
+
+function MoviesList(props) {
+  const { movies } = props;
+
+  if (!movies) return <div className="main-view" />;
+
+  return (
+    <Col md={4} key={m._id}>
+      <MovieCard movie={m} />
+    </Col>
+  );
+}
+
