701615a2537aace301226970c813bc149500f6c1
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-07-29 18:17:33 +0400

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import { Col, Row, Container } from "react-bootstrap";
 
-import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from "../movie-card/movie-card";
+
+const mapStateToProps = (state) => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
 
 function MoviesList(props) {
-  const { movies } = props;
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((m) => m.Title.toLocaleLowerCase().includes(visibilityFilter.toLocaleLowerCase()));
+  }
 
   if (!movies) return <div className="main-view" />;
 
   return (
-    <Col md={4} key={m._id}>
-      <MovieCard movie={m} />
-    </Col>
+    <Container>
+      <Row className="main-cardContainer">
+        {filteredMovies.map((m, index) => (
+          <Col key={index} className="main-card" lg="3" md="4" sm="6" xs="10">
+            <MovieCard key={m._id} movie={m} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
-export default MoviesList
+export default connect(mapStateToProps)(MoviesList);
