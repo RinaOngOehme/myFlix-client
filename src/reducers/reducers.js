38a42d3dce0ff1a1f3b9e10217066cad1d5edaa0
RinaOngOehme <81406377+RinaOngOehme@users.noreply.github.com>
2021-08-08 08:49:46 +0400

    Update reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, SET_FAVORITES, ADD_FAVORITE, DELETE_FAVORITE, DELETE_USER, UPDATE_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -21,20 +21,46 @@ function movies(state = [], action) {
 }
 
 function user(state = [], action) {
-    switch (action.type) {
-        case SET_USER:
-            console.log('SET_USER reducer called');
-            return action.value
-        default:
-            return state;
+  switch (action.type) {
+    case SET_USER:
+      console.log('SET_USER reducer called');
+      return action.value
 
-    }
+    case UPDATE_USER:
+      return action.value
+
+    case DELETE_USER:
+      return action.value
+
+    default:
+      return state;
+  }
 }
+
+function currentFavorites(state = [], action) {
+  switch (action.type) {
+    case SET_FAVORITES:
+      return action.value;
+    case ADD_FAVORITE:
+      let addedMovieState = state.concat(action.value);
+      return addedMovieState;
+    case DELETE_FAVORITE:
+      let deletedMovieState = state.filter((m) => m !== action.value);
+      return deletedMovieState;
+    default:
+      return state;
+  }
+}
+
+
+
 
 const moviesApp = combineReducers({
   visibilityFilter,
   movies,
-  user
+  user,
+  currentFavorites,
+
 });
 
 export default moviesApp;
